58d82b0056a7f3e4f23b9ace63e349c1e0792f1c
Michael Meskes <meskes@debian.org>
2013-04-10 20:01:37 +0200

    Cleaning up temp check code.
    Partly by Paul Crawford <psc@sat.dundee.ac.uk>

--- src/temp.c
+++ src/temp.c
@@ -13,6 +13,10 @@
 
 static int temp_fd = -1;
 
+static int templevel1, have1;
+static int templevel2, have2;
+static int templevel3, have3;
+
 /* ================================================================= */
 
 int open_tempcheck(char *name)
@@ -27,6 +31,28 @@ int open_tempcheck(char *name)
 		} else {
 			rv = 0;
 		}
+
+		/*
+		 * Clear flags and set/compute warning and max thresholds. Make
+		 * sure that each level is distinct and properly orderd so that
+		 * we have templevel1 < templevel2 < templevel3 < maxtemp
+		 */
+		have1 = have2 = have3 = FALSE;
+
+		templevel3 = (maxtemp * 98) / 100;
+		if (templevel3 >= maxtemp) {
+			templevel3 = maxtemp - 1;
+		}
+
+		templevel2 = (maxtemp * 95) / 100;
+		if (templevel2 >= templevel3) {
+			templevel2 = templevel3 - 1;
+		}
+
+		templevel1 = (maxtemp * 90) / 100;
+		if (templevel1 >= templevel2) {
+			templevel1 = templevel2 - 1;
+		}
 	}
 
 	return rv;
@@ -37,9 +63,6 @@ int open_tempcheck(char *name)
 int check_temp(void)
 {
 	unsigned char temperature;
-	int templevel1 = maxtemp * 9 / 10, have1 = FALSE;
-	int templevel2 = maxtemp * 95 / 100, have2 = FALSE;
-	int templevel3 = maxtemp * 98 / 100, have3 = FALSE;
 
 	/* is the temperature device open? */
 	if (temp_fd == -1)
@@ -59,34 +82,35 @@ int check_temp(void)
 	if (verbose && logtick && ticker == 1)
 		log_message(LOG_INFO, "current temperature is %d", temperature);
 
+	/* Print out warnings as we cross the 90/95/98 percent thresholds. */
 	if (temperature > templevel3) {
 		if (!have3) {
-			/* once we reach level3, issue a warning */
-			log_message(LOG_WARNING, "temperature increases %d", templevel3);
+			/* once we reach level3, issue a warning once. */
+			log_message(LOG_WARNING, "temperature increases above %d", templevel3);
 			have1 = have2 = have3 = TRUE;
 		}
-	} else {
-		have3 = FALSE;
-		if (temperature > templevel2) {
-			if (!have2) {
-				/* once we reach level2, issue a warning */
-				log_message(LOG_WARNING, "temperature increases %d", templevel2);
-				have1 = have2 = TRUE;
-			}
-		} else {
-			have2 = have3 = FALSE;
-			if (temperature > templevel1) {
-				if (!have1) {
-					/* once we reach level1, issue a warning */
-					log_message(LOG_WARNING, "temperature increases %d", templevel1);
-					have1 = TRUE;
-				}
-			}
+	} else if (temperature > templevel2) {
+		if (!have2) {
+			log_message(LOG_WARNING, "temperature increases above %d", templevel2);
+			have1 = have2 = TRUE;
 		}
+		have3 = FALSE;
+	} else if (temperature > templevel1) {
+		if (!have1) {
+			log_message(LOG_WARNING, "temperature increases above %d", templevel1);
+			have1 = TRUE;
+		}
+		have2 = have3 = FALSE;
+	} else {
+		/* Below all thresholds, report clear only if previously set. */
+		if (have1 || have2 || have3) {
+			log_message(LOG_INFO, "temperature now OK again");
+		}
+		have1 = have2 = have3 = FALSE;
 	}
 
 	if (temperature >= maxtemp) {
-		log_message(LOG_ERR, "it is too hot inside (temperature = %d)", temperature);
+		log_message(LOG_ERR, "it is too hot inside (temperature = %d >= %d)", temperature, maxtemp);
 		return (ETOOHOT);
 	}
 	return (ENOERR);
