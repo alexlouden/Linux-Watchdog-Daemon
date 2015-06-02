3df7e5a0608330558f95e8e15fcbce142663e33f
Paul Crawford <paulcrawford@users.sourceforge.net>
2015-06-02 20:54:13 +0100

    Minor tidy-up of network interface checking code

--- src/iface.c
+++ src/iface.c
@@ -13,11 +13,12 @@
 
 int check_iface(struct list *dev)
 {
-	FILE *file = fopen("/proc/net/dev", "r");
+	const char fname[] = "/proc/net/dev";
+	FILE *file = fopen(fname, "r");
 
 	if (file == NULL) {
 		int err = errno;
-		log_message(LOG_ERR, "cannot open /proc/net/dev (errno = %d = '%s')", err, strerror(err));
+		log_message(LOG_ERR, "cannot open %s (errno = %d = '%s')", fname, err, strerror(err));
 
 		if (softboot)
 			return (err);
@@ -28,13 +29,14 @@ int check_iface(struct list *dev)
 	/* read the file line by line */
 	while (!feof(file)) {
 		char line[NETDEV_LINE_LEN];
+		memset(line, 0, sizeof(line)); /* Just in case. */
 
 		if (fgets(line, NETDEV_LINE_LEN, file) == NULL) {
-			if (!ferror(file))
+			if (!ferror(file)) {
 				break;
-			else {
+			} else {
 				int err = errno;
-				log_message(LOG_ERR, "cannot read /proc/net/dev (errno = %d = '%s')", err, strerror(err));
+				log_message(LOG_ERR, "cannot read %s (errno = %d = '%s')", fname, err, strerror(err));
 
 				fclose(file);
 				if (softboot)
@@ -51,22 +53,23 @@ int check_iface(struct list *dev)
 
 				/* do verbose logging */
 				if (verbose && logtick && ticker == 1)
-					log_message(LOG_INFO, "device %s received %lu bytes", dev->name, bytes);
+					log_message(LOG_DEBUG, "device %s received %lu bytes", dev->name, bytes);
 
 				if (dev->parameter.iface.bytes == bytes) {
 					fclose(file);
 					log_message(LOG_ERR, "device %s did not receive anything since last check", dev->name);
 
 					return (ENETUNREACH);
-				} else
+				} else {
 					dev->parameter.iface.bytes = bytes;
+				}
 			}
 		}
 	}
 
 	if (fclose(file) != 0) {
 		int err = errno;
-		log_message(LOG_ERR, "cannot close /proc/net/dev (errno = %d = '%s')", err, strerror(err));
+		log_message(LOG_ERR, "cannot close %s (errno = %d = '%s')", fname, err, strerror(err));
 
 		if (softboot)
 			return (err);
